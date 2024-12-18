9d76d79bcab08bd34a19d86efdf9a76f0c00bb1e
Maksym Yefimov <yefimov.maksym@lll.kpi.ua>
2024-12-18 02:15:31 +0200

    first commit

---
+++ src/MatrixOperations.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class MatrixOperations {
+
+    public static void main(String[] args) {
+        // Ініціалізована матриця
+        long[][] matrix = {
+                {54, 5662, 1563, 100},
+                {456, 509843, 16, 24},
+                {7777, 80, 910, 75}
+        };
+
+        // Транспонування матриці
+        long[][] transposedMatrix = transposeMatrix(matrix);
+        System.out.println("Оригінальна матриця:");
+        printMatrix(matrix);
+        System.out.println("Транспонована матриця:");
+        printMatrix(transposedMatrix);
+
+        // Підрухунок суми найменших елементів в кожній колонці
+        long sumOfSmallestElements = sumOfSmallestElementsInColumns(transposedMatrix);
+        System.out.println("Sum of the smallest elements in each column: " + sumOfSmallestElements);
+    }
+
+    // Транспонування
+    public static long[][] transposeMatrix(long[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        long[][] transposed = new long[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
+    }
+
+    // Обчислення суми найменших елементів у кожному стовпці матриці
+    public static long sumOfSmallestElementsInColumns(long[][] matrix) {
+        int cols = matrix[0].length;
+        long sum = 0;
+
+        for (int j = 0; j < cols; j++) {
+            long min = Long.MAX_VALUE;
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+            sum += min;
+        }
+
+        return sum;
+    }
+
+    // Виведення матриці в консоль
+    public static void printMatrix(long[][] matrix) {
+        for (long[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
